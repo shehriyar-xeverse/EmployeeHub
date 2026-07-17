d9d6d64f0ca98b386a1524989b5df7b7f90708e6
shehriyar-xeverse <sheriyar.a@xeverse.io>
2026-07-17 22:15:48 +0500

    update socket config

--- socket/socket.ts
+++ socket/socket.ts
@@ -4,4 +4,5 @@ import { io } from "socket.io-client";
 export const socket = io(process.env.NEXT_PUBLIC_BACKEND_URL, {
     withCredentials : true,
     autoConnect : false,
+    transports: ["websocket", "polling"]
 })
